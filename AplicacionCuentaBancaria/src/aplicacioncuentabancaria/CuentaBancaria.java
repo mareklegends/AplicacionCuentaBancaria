47555c0b6d39333fc8ddc382e12fad42f58f3738
alumno <alumno@aula1-02>
2018-11-23 18:30:02 +0100

    version 1

--- AplicacionCuentaBancaria/src/aplicacioncuentabancaria/CuentaBancaria.java
+++ AplicacionCuentaBancaria/src/aplicacioncuentabancaria/CuentaBancaria.java
@@ -11,4 +11,26 @@ package aplicacioncuentabancaria;
  */
 public class CuentaBancaria {
     
+    private String nombre_titular;
+    
+    private String entidad;
+    private String oficina;
+    private String digitos_de_control;
+    private String cuenta;
+    
+    //añadir una cuenta
+
+    public CuentaBancaria(String nombre_titular, String entidad, String oficina, String digitos_de_control, String cuenta) {
+        this.nombre_titular = nombre_titular;
+        
+        this.entidad = entidad;
+        this.oficina = oficina;
+        this.digitos_de_control = digitos_de_control;
+        this.cuenta = cuenta;
+    }
+    
+    
+    
+    
+    
 }
